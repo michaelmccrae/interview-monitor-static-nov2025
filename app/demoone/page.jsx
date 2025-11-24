f517fccd56e1d01881e4922ece2d446600b83909
michaelmccrae <mike@mccrae.ca>
2025-11-24 11:01:35 -0800

    dumped more files

---
+++ app/demoone/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Version from './verthree'
+
+const page = () => {
+  return (
+    <div className = "w-5/8"><Version /></div>
+  )
+}
+
+export default page
---
+++ app/workingone/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import APICalls from './apicalls'
+// import Utter from './testutterances'
+
+
+const page = () => {
+  return (
+    <div className="p-6 w-1/2"><APICalls /></div>
+    // <div><PretendFeed /></div>
+    // <div><Utter /></div>
+  )
+}
+
+export default page
